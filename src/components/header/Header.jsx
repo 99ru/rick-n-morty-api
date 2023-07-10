831ddad03ddcfceb04f2f4e8b14f2ad5e52f3ed1
99ru <yan.99ru@gmail.com>
2023-07-10 20:44:02 +0200

    remove logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,8 @@
 import './Header.css'
-import Logo from '../../assets/svg.png'
 
 const Header = () => {
   return (
     <header>
-      {/* <img src={Logo} alt="Rick and Morty" className="header-logo" /> */}
       <h1>Rick & Morty Wiki</h1>
     </header>
   )
